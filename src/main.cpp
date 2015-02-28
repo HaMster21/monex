36b2a981dc95094f9ee50c273d43b46c013423ba
Hans Meyer <hamster.of.dev@gmail.com>
2015-02-28 16:27:50 +0000

    Introducing boost
    The project is configured now to include boost.asio and dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,10 @@
  */
 
 #include <iostream>
+#include <boost/cerrno.hpp>
+#include <boost/asio.hpp>
 
 int main() {
   std::cout << "Hello world!\n";
+  return boost::system::error_code().value();
 }
